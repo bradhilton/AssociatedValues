bb7730e8fbe376cf053ee5add359ea71f2207f60
bradhilton <brad.hilton.nw@gmail.com>
2016-02-19 09:08:22 -0700

    Initial Commit

---
+++ AssociatedValues/AssociatedValues.h
@@ -0,0 +1,19 @@
+//
+//  AssociatedValues.h
+//  AssociatedValues
+//
+//  Created by Bradley Hilton on 2/19/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AssociatedValues.
+FOUNDATION_EXPORT double AssociatedValuesVersionNumber;
+
+//! Project version string for AssociatedValues.
+FOUNDATION_EXPORT const unsigned char AssociatedValuesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssociatedValues/PublicHeader.h>
+
+
